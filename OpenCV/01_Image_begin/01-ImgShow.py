cc6b5fcc9a306ddae89471495811267e2c5d7379
kimkyeonmo <a01055323826@gmail.com>
2025-08-12 21:05:53 +0900

    no message

---
+++ OpenCV/01_Image_begin/01-ImgShow.py
@@ -0,0 +1,17 @@
+import os
+import cv2
+
+
+path = '../image/data-jpg'
+fname = os.path.join(path, "lena.jpg")
+
+original = cv2.imread(fname, cv2.IMREAD_COLOR)
+gray = cv2.imread(fname, cv2.IMREAD_GRAYSCALE)
+unchange = cv2.imread(fname, cv2.IMREAD_UNCHANGED)
+
+cv2.imshow('Original', original)
+cv2.imshow('Gray', gray)
+cv2.imshow('Unchange', unchange)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
